2c1639992754b4bfe6c0525b3ff029aae82c38a6
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2024-05-29 17:36:47 +0200

    feat: new ranking process

--- api/app.py
+++ api/app.py
@@ -1,4 +1,6 @@
+import math
 import os
+from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
@@ -22,33 +24,42 @@ def find_most_relevant_items(words, limit=10):
     if not words:
         return []
 
-    like_conditions = [Token.word.like(f"%{word}%") for word in words]
+    like_conditions = [Token.word.ilike(f"%{word}%") for word in words]
 
     if len(like_conditions) == 1:
         conditions = like_conditions[0]
     else:
         conditions = or_(*like_conditions)
 
+    current_date = datetime.now()
     subquery = session.query(
         Token.item_id,
-        func.sum(Token.rank).label('total_rank')
+        func.sum(Token.rank).label('total_rank'),
+        func.max(Item.pub_date).label('max_pub_date')
     ).filter(
         conditions
+    ).join(
+        Item, Token.item_id == Item.hashcode
     ).group_by(
         Token.item_id
     ).subquery()
 
+    date_weight = 1
     query = session.query(
-        Item
+        Item,
+        (subquery.c.total_rank - func.log(date_weight * func.extract('epoch',
+                                                                     current_date - subquery.c.max_pub_date) / 86400)).label(
+            'weighted_score')
     ).join(
         subquery, Item.hashcode == subquery.c.item_id
     ).order_by(
-        desc(subquery.c.total_rank)
+        desc('weighted_score')
     ).limit(limit)
 
     most_relevant_items = query.all()
+    session.close()
 
-    return most_relevant_items
+    return [item for item, _ in most_relevant_items]
 
 
 @app.route('/search')
@@ -59,7 +70,6 @@ def search():
         return jsonify({"error": "No query provided"}), 400
 
     query_string = string_utils.ProcessingString().process_text(query_string)
-    # print(query_string)
     words = query_string.split(' ')
     limit = int(request.args.get('limit', 10))
 
