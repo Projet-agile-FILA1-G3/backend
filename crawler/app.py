1e89ef577935a6759eacf553c01435d4975f0c41
eliasmorio <83926043+eliasmorio@users.noreply.github.com>
2024-05-30 10:49:03 +0200

    add logs

--- crawler/app.py
+++ crawler/app.py
@@ -1,3 +1,4 @@
+import logging
 from os import getenv
 from time import sleep
 
@@ -12,6 +13,8 @@ method_crawler = getenv('CRAWLER_METHOD')
 schedule.every(sleeping_time).seconds.do(lambda: Crawler(method_crawler).crawl())
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Crawler started")
     while True:
         schedule.run_pending()
         sleep(1)
