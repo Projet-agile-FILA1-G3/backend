bb598dbe9dbb1071e246b90d2d08fe2d6f616b8e
unknown <ruben@backcar.fr>
2024-05-30 09:11:02 +0200

    [ADD] Explorer à finir => rajouter la méthode proscess_page de crawler_class.py

--- crawler/crawler_class.py
+++ crawler/crawler_class.py
@@ -7,16 +7,12 @@ from rss_parser.models.atom.feed import Tag, Entry
 import rss_parser.models.rss.channel as RSS_tag
 
 from sqlalchemy.exc import IntegrityError
-
+from explorer import Explorer
 from shared.db import get_session
 from shared.models import Rss, Item
 from token_management import ProcessingToken
 
 from datetime import datetime, timedelta
-# Si csv est choisi comme source, alors un fichier urls.csv doit se situer au même endroit que
-# la classe Crawler.
-#
-#
 
 
 class Crawler:
@@ -44,8 +40,85 @@ class Crawler:
         return text.split("content='")[0].split("' attributes")[0]
 
     @staticmethod
-    def recursive_crawling(urls, start_url, target_url):
-        return None
+    def process_page(url, headers="curl/7.64.1"):
+        response = get(url, headers={"User-Agent": headers})
+        try:
+            item_list = RSSParser.parse(response.text, schema=RSS).channel.items
+            rss_info = RSSParser.parse(response.text, schema=RSS).channel
+            rss_title = rss_info.title.content
+            rss_language = rss_info.language.content
+            rss_url = response.url
+            try:
+                rss_description = rss_info.description.content
+            except:
+                rss_description = None
+            try:
+                rss_lastdate = rss_info.pub_date.content.isoformat()
+            except:
+                try:
+                    rss_lastdate = rss_info.last_build_date.isoformat()
+                except:
+                    rss_lastdate = None
+            rss_dict = dict({
+                'language': rss_language,
+                'title': rss_title,
+                'updated': rss_lastdate,
+                'url': rss_url,
+                'description': rss_description
+            })
+            return rss_dict, item_list
+        except Exception as primeError:
+            # print("Attention erreur ici :", primeError)
+            try:
+                item_list = BaseParser.parse(response.text, schema=Atom).feed.content.entries
+                atom_info = BaseParser.parse(response.text, schema=Atom).feed.content
+                atom_language = 'en'
+                atom_title = atom_info.title.content
+                atom_updated = atom_info.updated.content
+                atom_url = response.url
+                rss_dict = dict({
+                    'language': atom_language,
+                    'title': atom_title,
+                    'updated': atom_updated,
+                    'url': atom_url,
+                    'description': None
+                })
+                return rss_dict, item_list
+            except Exception as error:
+                return None, []
+
+
+    @staticmethod
+    def process_items(item_list, url):
+        items = []
+        try:
+            if type(item_list[0]) == Tag[Entry]:
+                for item in item_list:
+                    if item.content.content != None:
+                        items.append(dict({
+                            'title': Crawler.split_content(item.content.title),
+                            'description': Crawler.split_content(item.content.content),
+                            'source_url': url,
+                            'url': item.content.links[0].attributes['href'],
+                            'pub_date': Crawler.parse_date(item, 'atom')
+                        }))
+                        Explorer(url, item.content.links[0].attributes['href'])
+                return items
+            if type(item_list[0]) == RSS_tag.Tag[RSS_tag.Item]:
+                for item in item_list:
+                    if item.description != None:
+                        items.append(dict({
+                            'title': Crawler.split_content(item.title),
+                            'description': Crawler.split_content(item.description),
+                            'source_url': url,
+                            'url': item.link.content,
+                            'pub_date': Crawler.parse_date(item, 'rss')
+                        }))
+                        Explorer(url, item.link.content)
+                return items
+        except Exception as error:
+            print("Fatal Error :", error)
+            return []
 
     def __init__(self, RSS_URLS_TYPE = 'csv', STORAGE_TYPE = 'postgres', DEF_URL = None):
         self.source_type = RSS_URLS_TYPE.lower()
@@ -115,79 +188,13 @@ class Crawler:
                 except IntegrityError:
                     session.rollback()
                     continue
+        session.close()
 
     def crawl(self):
-        for urls in self.urls:
-            items = []
-            response = get(urls, headers={"User-Agent": "curl/7.64.1"})
-            try:
-                item_list = RSSParser.parse(response.text, schema=RSS).channel.items
-                rss_info = RSSParser.parse(response.text, schema=RSS).channel
-                # print(rss_info)
-                rss_title = rss_info.title.content
-                rss_language = rss_info.language.content
-                rss_lastdate = None
-                rss_description = None
-                rss_url = response.url
-                try:
-                    rss_description = rss_info.description.content
-                except:
-                    rss_description = None
-                try:
-                    rss_lastdate = rss_info.pub_date.content.isoformat()
-                except:
-                    try:
-                        rss_lastdate = rss_info.last_build_date.isoformat()
-                    except:
-                        rss_lastdate = None
-                rss_dict = dict({
-                    'language': rss_language,
-                    'title': rss_title,
-                    'updated': rss_lastdate,
-                    'url': rss_url,
-                    'description': rss_description
-                })
-            except Exception as primeError:
-                # print("Attention erreur ici :", primeError)
-                try:
-                    item_list = BaseParser.parse(response.text, schema=Atom).feed.content.entries
-                    atom_info = BaseParser.parse(response.text, schema=Atom).feed.content
-                    atom_language = 'en'
-                    atom_title = atom_info.title.content
-                    atom_updated = atom_info.updated.content
-                    atom_url = response.url
-                    rss_dict = dict({
-                        'language': atom_language,
-                        'title': atom_title,
-                        'updated': atom_updated,
-                        'url': atom_url,
-                        'description': None
-                    })
-                except Exception as error:
-                    return 0
-            try:
-                if type(item_list[0]) == Tag[Entry]:
-                    for item in item_list:
-                        if item.content.content != None:
-                            items.append(dict({
-                                'title': Crawler.split_content(item.content.title),
-                                'description': Crawler.split_content(item.content.content),
-                                'source_url': response.url,
-                                'url': item.content.links[0].attributes['href'],
-                                'pub_date': Crawler.parse_date(item, 'atom')
-                            }))
-                if type(item_list[0]) == RSS_tag.Tag[RSS_tag.Item]:
-                    for item in item_list:
-                        if item.description != None:
-                            items.append(dict({
-                                'title': Crawler.split_content(item.title),
-                                'description': Crawler.split_content(item.description),
-                                'source_url': response.url,
-                                'url': item.link.content,
-                                'pub_date': Crawler.parse_date(item, 'rss')
-                            }))
+        for url in self.urls:
+            rss_dict, item_list = Crawler.process_page(url)
+            items = Crawler.process_items(item_list, url)
+            self.save_data(rss_dict, items)
 
-                self.save_data(rss_dict, items)
-            except Exception as error:
-                print("Fatal Error :", error)
-                pass
+if __name__ == '__main__':
+    Crawler().crawl()
