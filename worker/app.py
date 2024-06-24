09eed68707915d575204cf8a8b10c5b83e37e219
eliasmorio <83926043+eliasmorio@users.noreply.github.com>
2024-06-24 11:04:46 +0200

    feat: add insert defaults feed if no one in db

--- worker/app.py
+++ worker/app.py
@@ -1,3 +1,4 @@
+import csv
 import logging
 import os
 
@@ -7,10 +8,13 @@ from time import sleep
 
 import schedule
 
-from shared.db import init_db
+from shared.db import init_db, get_session
+from shared.models.Feed import Feed
+from shared.persistence.FeedRepository import FeedRepository
 from worker.scheduler import scheduler_init
+from utils import is_prod_env, add_defaults_feed
 
-if os.getenv('ENV') != 'production':
+if not is_prod_env():
     from dotenv import load_dotenv
     load_dotenv()
 
@@ -27,8 +31,11 @@ if __name__ == '__main__':
 
     try:
         init_db()
+        if not is_prod_env():
+            add_defaults_feed()
+
     except:
-        logging.error(f"Failed to initialize database: {os.getenv('POSTGRES_HOST')}")
+        logging.error(f"Failed to initialize database: {os.getenv('POSTGRES_HOST')}", exc_info=True)
         exit(1)
 
     # Init APP
