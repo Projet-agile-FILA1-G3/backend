1217f0962751c40b3a3d90788acbfeb6d987a357
rubensailly <105773429+rubensailly@users.noreply.github.com>
2024-06-27 08:36:15 +0200

    RR-52 Images dans les content (#19)
    * [ADD] Récupération si possible d'un lien audio sur un flux RSS (et non ATOM)
    
    * [FIX] Liens Podcasts
    
    * [ADD] Récupération d'image dans les content des flux
    
    ---------
    
    Co-authored-by: unknown <ruben@backcar.fr>

--- worker/parsing/item_parsing.py
+++ worker/parsing/item_parsing.py
@@ -1,12 +1,20 @@
+import logging
 from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
-
+from bs4 import BeautifulSoup
 from shared.models.Item import Item
-
+import html
 
 def split_content(text):
-    return text.split("content='")[0].split("' attributes")[0]
-
+    text = text.split("content='")[0].split("' attributes")[0]
+    return text
+    """
+    try:
+        decoded_text = BeautifulSoup(text).getText()
+        picture_link = BeautifulSoup(text).find('picture')['src']
+        return decoded_text, picture_link
+    except:
+        return text"""
 
 class ItemParser(ABC):
     def __init__(self, parsed_item, feed_id):
@@ -20,7 +28,8 @@ class ItemParser(ABC):
             link=self.get_link(),
             pub_date=self.get_pub_date(),
             feed_id=self.feed_id,
-            audio_link=self.get_audio()
+            audio_link=self.get_audio(),
+            image_link=self.get_image()
         )
         return item
 
@@ -40,6 +49,11 @@ class ItemParser(ABC):
     def get_audio(self):
         pass
 
+
+    @abstractmethod
+    def get_image(self):
+        pass
+      
     def get_pub_date(self):
         return (datetime.now() - timedelta(days=2)).isoformat()
 
@@ -55,7 +69,12 @@ class RssItemParser(ItemParser):
     def get_description(self):
         if not self.item.description:
             raise Exception("No description")
-        return split_content(self.item.description)
+        content = split_content(self.item.description)
+        try:
+            decoded_text = BeautifulSoup(content).getText()
+            return decoded_text
+        except:
+            return content
 
     def get_link(self):
         try:
@@ -69,6 +88,18 @@ class RssItemParser(ItemParser):
                 return self.item.enclosure.attributes['url']
         except:
             return None
+
+    def get_image(self):
+        try:
+            picture_link = None
+            if BeautifulSoup(html.unescape(split_content(self.item.description)), features='html.parser').find('picture'):
+                picture_link = BeautifulSoup(html.unescape(split_content(self.item.description)), features='html.parser').find('picture')['src']
+            if BeautifulSoup(html.unescape(split_content(self.item.description)), features='html.parser').find('img'):
+                picture_link = BeautifulSoup(html.unescape(split_content(self.item.description)), features='html.parser').find('img')['src']
+            return picture_link
+        except:
+            return None
+
 
     def get_pub_date(self):
         if not self.item.pub_date:
@@ -90,7 +121,12 @@ class AtomItemParser(ItemParser):
         return split_content(self.item.content.title)
 
     def get_description(self):
-        return split_content(self.item.content.content)
+        content = split_content(self.item.content.content)
+        try:
+            decoded_text = BeautifulSoup(content).getText()
+            return decoded_text
+        except:
+            return content
 
     def get_link(self):
         return self.item.content.links[0].attributes['href']
@@ -98,6 +134,18 @@ class AtomItemParser(ItemParser):
     def get_audio(self):
         # TODO : Il faut que l'on teste avec du atom et du podcast
         return None
+
+    def get_image(self):
+        try:
+            picture_link = None
+            if BeautifulSoup(html.unescape(split_content(self.item.content.content)), features='html.parser').find('picture'):
+                picture_link = BeautifulSoup(html.unescape(split_content(self.item.content.content)), features='html.parser').find('picture')['src']
+            if BeautifulSoup(html.unescape(split_content(self.item.content.content)), features='html.parser').find('img'):
+                picture_link = BeautifulSoup(html.unescape(split_content(self.item.content.content)), features='html.parser').find('img')['src']
+            return picture_link
+        except:
+            return None
+
 
     def get_pub_date(self):
         if not self.item.content.updated:
