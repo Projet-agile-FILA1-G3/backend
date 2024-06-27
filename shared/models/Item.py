f13dc850ceba31c898e1624e0e8eb1d44a90a473
ruben sailly <ruben.sailly@gmail.com>
2024-06-27 07:55:18 +0200

    [ADD] Récupération d'image dans les content des flux

--- shared/models/Item.py
+++ shared/models/Item.py
@@ -13,13 +13,14 @@ class Item(Base):
     description = Column(String, nullable=False)
     link = Column(String, nullable=False)
     audio_link = Column(String, nullable=True)
+    image_link = Column(String, nullable=True)
     pub_date = Column(DateTime, nullable=False)
     feed_id = Column(UUID(as_uuid=True), ForeignKey('RR_FEED.id'), nullable=False)
 
     feed = relationship("Feed", back_populates="items")
     tokens = relationship("Token", back_populates="item", cascade="all, delete-orphan")
 
-    def __init__(self, title, description, link, pub_date, feed_id, audio_link=None, **kw):
+    def __init__(self, title, description, link, pub_date, feed_id, audio_link=None, image_link=None, **kw):
         super().__init__(**kw)
         self.hashcode = hashlib.md5((title + description + link).encode('utf-8')).hexdigest()
         self.title = title
@@ -28,3 +29,4 @@ class Item(Base):
         self.pub_date = pub_date
         self.feed_id = feed_id
         self.audio_link = audio_link
+        self.image_link = image_link
